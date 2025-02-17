32622b79a73d5d0aaef78afd025e764ed56a59a4
Ethan Bickel <notethanbickel@gmail.com>
2025-02-17 15:40:55 -0600

    improve ui/ux for tag Swapping admin tool

--- src/app/admin/tags/tagSwapper.tsx
+++ src/app/admin/tags/tagSwapper.tsx
@@ -7,34 +7,51 @@ export default function TagSwapper() {
   const [newTag, setNewTag] = useState('');
   const mutate = api.club.changeTags.useMutation();
   return (
-    <div>
+    <div className="rounded-lg bg-white p-5">
       <div>
-        <h2>Old tag</h2>
+        <label htmlFor="oldTag">
+          <h2 className="text-lg font-bold">Old tag</h2>
+        </label>
         <input
           type="text"
           value={oldTag}
+          id="oldTag"
+          className="rounded-lg border-2 border-black p-1"
           onChange={(e) => {
             setOldTag(e.target.value);
           }}
         />
       </div>
       <div>
-        <h2>New tag</h2>
+        <label htmlFor="newTag">
+          <h2 className="text-lg font-bold">New tag</h2>
+        </label>
         <input
           type="text"
           value={newTag}
+          id="newTag"
+          className="rounded-lg border-2 border-black p-1"
           onChange={(e) => {
             setNewTag(e.target.value);
           }}
         />
       </div>
-      <button
-        onClick={() => {
-          mutate.mutate({ oldTag: oldTag, newTag: newTag });
-        }}
-      >
-        change tags
-      </button>
+      <div className="mt-2 flex w-full">
+        <button
+          className="ml-auto rounded-lg bg-blue-primary p-2 text-lg font-bold text-white disabled:opacity-50"
+          disabled={mutate.isPending}
+          onClick={() => {
+            mutate.mutate({ oldTag: oldTag, newTag: newTag });
+          }}
+        >
+          change tags
+        </button>
+      </div>
+      {mutate.isSuccess && (
+        <div className="mt-2 font-semibold text-blue-primary">
+          Modified the tags for {mutate.data.affected} clubs
+        </div>
+      )}
     </div>
   );
 }
