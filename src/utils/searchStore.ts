df699d7e91329863b8f1d9884af1cb58e763523f
Ethan Bickel <notethanbickel@gmail.com>
2025-03-07 19:27:58 -0600

    add zustand

---
+++ src/utils/searchStore.ts
@@ -0,0 +1,24 @@
+import { createStore } from 'zustand/vanilla';
+export type SearchState = {
+  search: string;
+  tag: string;
+};
+
+export type SearchAction = {
+  setSearch: (search: SearchState['search']) => void;
+  setTag: (tag: SearchState['tag']) => void;
+};
+export const defaultInitState: SearchState = {
+  search: '',
+  tag: 'All',
+};
+export type SearchStore = SearchState & SearchAction;
+export const createSearchStore = (
+  initState: SearchState = defaultInitState,
+) => {
+  return createStore<SearchStore>((set) => ({
+    ...initState,
+    setSearch: (search: string) => set(() => ({ search: search })),
+    setTag: (tag: string) => set(() => ({ tag: tag })),
+  }));
+};
