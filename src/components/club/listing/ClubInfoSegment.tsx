e400d98bab5ede01762aef187c1d3cb2e4ec7b07
Shivesh Gupta <ishaanshiveshhome@gmail.com>
2025-04-19 00:46:29 -0500

    Replace "active" field on club info with last updated (e.g. Feb 2024)

--- src/components/club/listing/ClubInfoSegment.tsx
+++ src/components/club/listing/ClubInfoSegment.tsx
@@ -6,7 +6,6 @@ import { api } from '@src/trpc/server';
 const ClubInfoSegment: FC<{
   club: NonNullable<RouterOutputs['club']['getDirectoryInfo']>;
 }> = async ({ club }) => {
-  const isActive = await api.club.isActive({ id: club.id });
   return (
     <div className="w-full rounded-lg bg-slate-100 p-10">
       <div className="flex flex-col items-start justify-between md:flex-row">
@@ -20,18 +19,16 @@ const ClubInfoSegment: FC<{
           />
           <h1 className="mt-5 text-2xl font-medium">Description</h1>
           <div className="mt-5 flex w-36 justify-between">
-            <p className="text-sm text-slate-400">Name</p>
+            <p className="text-slate-400">Name</p>
             <p className="text-right text-sm text-slate-600">{club.name}</p>
           </div>
           <div className="mt-2 flex w-36 justify-between">
-            <p className="text-sm text-slate-400">Founded</p>
+            <p className="text-slate-400">Founded</p>
             <p className="text-right text-sm text-slate-600">May 2020</p>
           </div>
           <div className="mt-2 flex w-36 justify-between">
-            <p className="text-sm text-slate-400">Active</p>
-            <p className="text-right text-sm text-slate-600">
-              {isActive ? 'Present' : 'Inactive'}
-            </p>
+            <p className="text-slate-400">Updated</p>
+            <p className="text-right text-sm text-slate-600">February 2024</p>
           </div>
         </div>
         <div className="w-full">
