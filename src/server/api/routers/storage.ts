3885f269af0cb73efc5218373c3b12b57d906b01
Ethan Bickel <notethanbickel@gmail.com>
2025-07-22 22:12:45 -0500

    remove trpc post schema
    trpc doesn't really handle blobs well so working on new endpoint

--- src/server/api/routers/storage.ts
+++ src/server/api/routers/storage.ts
@@ -7,11 +7,6 @@ const getDeleteSchema = z.object({
   objectID: z.string(),
 });
 
-const postSchema = z.object({
-  objectID: z.string(),
-  data: z.file(),
-});
-
 export const storageRouter = createTRPCRouter({
   get: publicProcedure.input(getDeleteSchema).query(async ({ input }) => {
     const data = await callStorageAPI('GET', input.objectID);
@@ -26,17 +21,6 @@ export const storageRouter = createTRPCRouter({
   }),
   delete: protectedProcedure.input(getDeleteSchema).query(async ({ input }) => {
     const data = await callStorageAPI('DELETE', input.objectID);
-    if (data.message !== 'success') {
-      throw new TRPCError({
-        code: 'INTERNAL_SERVER_ERROR',
-        message: 'Nebula API errored on request',
-        cause: data,
-      });
-    }
-    return data;
-  }),
-  post: protectedProcedure.input(postSchema).query(async ({ input }) => {
-    const data = await callStorageAPI('POST', input.objectID, input.data);
     if (data.message !== 'success') {
       throw new TRPCError({
         code: 'INTERNAL_SERVER_ERROR',
