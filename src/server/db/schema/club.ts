83eba60b6d349ffc3899e441ccf7b27da58602c1
Tyler Hill <gotyhill@gmail.com>
2025-08-25 13:43:53 -0500

    Propper unique

--- src/server/db/schema/club.ts
+++ src/server/db/schema/club.ts
@@ -6,7 +6,7 @@ import {
   pgTable,
   pgView,
   text,
-  unique,
+  uniqueIndex,
 } from 'drizzle-orm/pg-core';
 import { events } from './events';
 import { userMetadataToClubs } from './users';
@@ -20,23 +20,31 @@ export const approvedEnum = pgEnum('approved_enum', [
   'pending',
 ]);
 
-export const club = pgTable('club', {
-  id: text('id')
-    .default(sql`nanoid(20)`)
-    .primaryKey(),
-  slug: text('slug').notNull().unique(),
-  name: text('name').notNull(),
-  description: text('description').notNull(),
-  tags: text('tags')
-    .array()
-    .default(sql`'{}'::text[]`)
-    .notNull(),
-  // * Approved will be null by default and will be set to true when the club is approved or false when the club is rejected
-  // * This allows us to have a pending state for clubs and keep info about them in the database
-  approved: approvedEnum('approved').notNull().default('pending'),
-  profileImage: text('profile_image'),
-  soc: boolean('soc').notNull().default(false),
-});
+export const club = pgTable(
+  'club',
+  {
+    id: text('id')
+      .default(sql`nanoid(20)`)
+      .primaryKey(),
+    slug: text('slug').notNull(),
+    name: text('name').notNull(),
+    description: text('description').notNull(),
+    tags: text('tags')
+      .array()
+      .default(sql`'{}'::text[]`)
+      .notNull(),
+    // * Approved will be null by default and will be set to true when the club is approved or false when the club is rejected
+    // * This allows us to have a pending state for clubs and keep info about them in the database
+    approved: approvedEnum('approved').notNull().default('pending'),
+    profileImage: text('profile_image'),
+    soc: boolean('soc').notNull().default(false),
+  },
+  (table) => {
+    return {
+      slugUnique: uniqueIndex('club_slug_unique').on(table.slug),
+    };
+  },
+);
 
 export const clubRelations = relations(club, ({ many }) => ({
   contacts: many(contacts),
