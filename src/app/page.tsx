b02db40f056bfe572b1fda523f91e840142c9044
Laura <laura.maya.tatar@gmail.com>
2025-09-29 19:38:44 -0500

    Fix formatting issues with Prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,25 +46,25 @@ const Home = async () => {
                 className="bg-no-repeat object-cover"
               />
               <Image
-                  src={planetsDoodle}
-                  //width={300}
-                  //height={300}
-                  alt="Planets Doodle for landing page"
-                  className="bg-no-repeat object-cover absolute bottom-[30%] right-[10%] w-[clamp(200px,20vw,300px)]"
+                src={planetsDoodle}
+                //width={300}
+                //height={300}
+                alt="Planets Doodle for landing page"
+                className="absolute right-[10%] bottom-[30%] w-[clamp(200px,20vw,300px)] bg-no-repeat object-cover"
               />
               <Image
-                  src={starDoodle}
-                  //width={170}
-                  //height={170}
-                  alt="Star Doodle (top right) for landing page"
-                  className="bg-no-repeat object-cover absolute top-[5%] right-[15%] w-[clamp(170px,20vw,200px)] animate-spin [animation-duration:77s] [animation-direction:reverse]"
+                src={starDoodle}
+                //width={170}
+                //height={170}
+                alt="Star Doodle (top right) for landing page"
+                className="absolute top-[5%] right-[15%] w-[clamp(170px,20vw,200px)] animate-spin bg-no-repeat object-cover [animation-direction:reverse] [animation-duration:77s]"
               />
               <Image
-                  src={starDoodle}
-                  //width={140}
-                  //height={140}
-                  alt="Star Doodle (upper right) for landing page"
-                  className="bg-no-repeat object-cover absolute bottom-[35%] left-[7%] w-[clamp(70px,20vw,150px)] animate-spin [animation-duration:60s]"
+                src={starDoodle}
+                //width={140}
+                //height={140}
+                alt="Star Doodle (upper right) for landing page"
+                className="absolute bottom-[35%] left-[7%] w-[clamp(70px,20vw,150px)] animate-spin bg-no-repeat object-cover [animation-duration:60s]"
               />
             </section>
             <section className="absolute top-[100vh] z-10 h-[20vh] w-screen bg-linear-to-t from-white to-transparent"></section>
