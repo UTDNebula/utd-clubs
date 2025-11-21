f3c79381574e8ebdb9306ecd707f7ba11296a2a3
SaivishaalSureshkannan <saivishaalsureshkannan@gmail.com>
2025-11-21 11:59:27 -0600

    Prevented ClubMatch button from overlapping with search bar on mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,9 +58,9 @@ const Home = async () => {
           </div>
         </div>
         <div className="relative inset-0 z-20 bg-transparent">
-          <div className="pointer-events-none *:pointer-events-auto fixed top-0 z-20 flex w-full items-center justify-between p-2.5 md:p-5">
+          <div className="pointer-events-none *:pointer-events-auto md:fixed md:top-0 md:z-20 flex w-full items center justify-between p-2.5 md:p-5">
             <Sidebar hamburger="white" shadow />
-            <div className="ml-auto flex items-center justify-center gap-2">
+            <div className="ml-auto flex items-center justify-center gap-2"> 
               <ClubMatchButton />
               <ProfileDropDown session={session} shadow />
             </div>
