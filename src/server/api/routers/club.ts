c31c73758ed5389bffdec8443bd9eae3aeaaa25c
Ethan Bickel <notethanbickel@gmail.com>
2025-12-02 19:56:53 -0600

    fix: missing import on club router

--- src/server/api/routers/club.ts
+++ src/server/api/routers/club.ts
@@ -1,4 +1,13 @@
-import { and, arrayOverlaps, asc, eq, ilike, inArray, sql } from 'drizzle-orm';
+import {
+  and,
+  arrayOverlaps,
+  asc,
+  desc,
+  eq,
+  ilike,
+  inArray,
+  sql,
+} from 'drizzle-orm';
 import { z } from 'zod';
 import { club, usedTags } from '@src/server/db/schema/club';
 import { contacts } from '@src/server/db/schema/contacts';
