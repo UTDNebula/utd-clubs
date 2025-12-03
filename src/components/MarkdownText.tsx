fa90b32ec4788bbd3fd552e7a1512ebf3e7da485
Tyler Hill <gotyhill@gmail.com>
2025-12-02 23:57:24 -0600

    Use @tailwindcss/typography

--- src/components/MarkdownText.tsx
+++ src/components/MarkdownText.tsx
@@ -4,7 +4,7 @@ import remarkGfm from 'remark-gfm';
 
 const MarkdownText = ({ text }: { text: string }) => {
   return (
-    <div className="markdown">
+    <div className="prose prose-slate prose-a:text-royal prose-ul:[&.contains-task-list]:list-none prose-ul:[&.contains-task-list]:pl-2">
       <Markdown
         remarkPlugins={[remarkGfm]}
         rehypePlugins={[
