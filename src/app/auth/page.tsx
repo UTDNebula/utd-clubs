b4bf93951cc63655def43efdf19725eee44951fb
Isosc <171582725+Isoscelestial@users.noreply.github.com>
2025-12-06 05:01:16 -0600

    Re-redesign `/auth` page

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,9 +1,13 @@
 import { headers } from 'next/headers';
 import Image from 'next/image';
+import Link from 'next/link';
 import { redirect } from 'next/navigation';
 import ProviderButton from '@src/app/auth/ProviderButtons';
+import RegisterModal, {
+  RegisterModalContents,
+} from '@src/components/account/RegisterModal';
 import BackButton from '@src/components/backButton';
-import RegisterModal from '@src/components/account/RegisterModal';
+import NebulaLogo from '@src/icons/NebulaLogo';
 import { auth } from '@src/server/auth';
 
 const providers = ['google', 'discord'] as const;
@@ -19,11 +23,26 @@ export default async function Auth(props: {
 
   return (
     <main className="h-screen">
-      <div className="relative flex h-screen basis-full flex-col items-center justify-center">
-        <div className="absolute inset-0 h-full w-full overflow-hidden">
-          <Image src={'/banner.png'} alt="background" fill objectFit="cover" />
+      <div className="relative flex h-screen basis-full flex-col items-center justify-center gap-8">
+        <div className="fixed inset-0 h-full w-full overflow-hidden">
+          <Image
+            src={'/banner.png'}
+            alt="background"
+            fill
+            objectFit="cover"
+            className="select-none"
+          />
         </div>
-        <RegisterModal open omitClose className='[&>.MuiModal-backdrop]:bg-transparent' />
+        <div className="z-10">
+          <Link
+            href="/"
+            className="lext-lg md:text-xl font-display font-medium md:font-bold flex gap-2 items-center text-white text-shadow-[0_0_4px_rgb(0_0_0_/_0.4)]"
+          >
+            <NebulaLogo className="h-6 w-auto fill-white drop-shadow-[0_0_4px_rgb(0_0_0_/_0.4)]" />
+            UTD CLUBS
+          </Link>
+        </div>
+        <RegisterModalContents />
         {/* <div className="flex flex-col gap-16">
           <div className="absolute inset-5 z-10 flex w-fit sm:static">
             <BackButton className="bg-white" />
