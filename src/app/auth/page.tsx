14b44e8a6cfe1c0e6ceb317c8bfb65f2b0e1ffab
Isosc <171582725+Isoscelestial@users.noreply.github.com>
2025-12-06 05:04:21 -0600

    Lint and format

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -2,15 +2,9 @@ import { headers } from 'next/headers';
 import Image from 'next/image';
 import Link from 'next/link';
 import { redirect } from 'next/navigation';
-import ProviderButton from '@src/app/auth/ProviderButtons';
-import RegisterModal, {
-  RegisterModalContents,
-} from '@src/components/account/RegisterModal';
-import BackButton from '@src/components/backButton';
+import { RegisterModalContents } from '@src/components/account/RegisterModal';
 import NebulaLogo from '@src/icons/NebulaLogo';
 import { auth } from '@src/server/auth';
-
-const providers = ['google', 'discord'] as const;
 
 export default async function Auth(props: {
   searchParams: Promise<{ [key: string]: string }>;
@@ -43,25 +37,6 @@ export default async function Auth(props: {
           </Link>
         </div>
         <RegisterModalContents />
-        {/* <div className="flex flex-col gap-16">
-          <div className="absolute inset-5 z-10 flex w-fit sm:static">
-            <BackButton className="bg-white" />
-          </div>
-          <div className="z-10 flex flex-col items-center justify-center space-y-12">
-            <h1 className="font-display text-center text-6xl font-bold text-white sm:text-left">
-              Sign In /<br className="sm:hidden" /> Sign Up
-            </h1>
-            <div className="flex w-full flex-col items-center justify-center space-y-4 gap-x-4 py-2.5 sm:flex-row sm:space-y-0">
-              {providers.map((provider) => (
-                <ProviderButton
-                  key={provider}
-                  provider={provider}
-                  callbackUrl={searchParams['callbackUrl']}
-                />
-              ))}
-            </div>
-          </div>
-        </div> */}
       </div>
     </main>
   );
