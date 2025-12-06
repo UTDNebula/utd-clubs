2c04879a721327ad57c8a12b1e953d3575ce57ff
Isosc <171582725+Isoscelestial@users.noreply.github.com>
2025-12-06 05:18:59 -0600

    Make ClubMatchButton open register modal

--- src/components/header/ClubMatchButton.tsx
+++ src/components/header/ClubMatchButton.tsx
@@ -2,13 +2,38 @@
 
 import { Button } from '@mui/material';
 import Link from 'next/link';
+import { authClient } from '@src/utils/auth-client';
+import {
+  NoRegisterModalProviderError,
+  useRegisterModalContext,
+} from '../account/RegisterModalProvider';
 
 export default function ClubMatchButton({ shadow }: { shadow?: boolean }) {
+  const { data: session } = authClient.useSession();
+
+  let setShowRegisterModal: (value: boolean) => void;
+  try {
+    const context = useRegisterModalContext();
+    setShowRegisterModal = context.setShowRegisterModal;
+  } catch (e) {
+    if (e instanceof NoRegisterModalProviderError) {
+    } else {
+      throw e;
+    }
+  }
+
   return (
-    <Link href="/club-match/results">
+    <Link href={(session && '/club-match/results') ?? ''}>
       <Button
         variant="contained"
         className={`rounded-full normal-case ${shadow ? 'drop-shadow-[0_0_4px_rgb(0_0_0_/_0.4)]' : ''}`}
+        onClick={
+          !session
+            ? () => {
+                setShowRegisterModal(true);
+              }
+            : undefined
+        }
       >
         Club Match
       </Button>
