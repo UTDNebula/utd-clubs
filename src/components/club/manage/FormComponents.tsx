8dd7347b42d0758de67ccc81c51c098f04b5c351
Ethan Bickel <notethanbickel@gmail.com>
2025-12-06 05:20:09 -0600

    fix: re-add react-hook-form buttons so that old forms compile

--- src/components/club/manage/FormComponents.tsx
+++ src/components/club/manage/FormComponents.tsx
@@ -1,2 +1,7 @@
 export { Form } from './form/Form';
 export { FormFieldSet } from './form/FormFieldSet';
+export {
+  FormSubmitButton,
+  FormResetButton,
+  FormButtons,
+} from './form/FormButtons';
--- src/components/club/manage/form/FormButtons.tsx
+++ src/components/club/manage/form/FormButtons.tsx
@@ -1,16 +1,80 @@
-import { withForm } from '@src/utils/form';
+import DeleteIcon from '@mui/icons-material/Delete';
+import SaveIcon from '@mui/icons-material/Save';
+import Button from '@mui/material/Button';
+import React from 'react';
+import { useFormContext } from 'react-hook-form';
 
-export const FormButtons = withForm({
-  render: function Render({ form }) {
-    return (
-      <div className="flex flex-wrap justify-end items-center gap-2">
-        <form.AppForm>
-          <form.FormResetButton />
-        </form.AppForm>
-        <form.AppForm>
-          <form.FormSubmitButton />
-        </form.AppForm>
-      </div>
-    );
-  },
-});
+export const FormSubmitButton = ({
+  disabled,
+  loading,
+}: {
+  disabled?: boolean;
+  loading?: boolean;
+}) => {
+  return (
+    <Button
+      type="submit"
+      variant="contained"
+      className="normal-case"
+      startIcon={<SaveIcon />}
+      disabled={disabled}
+      loading={loading}
+      loadingPosition="start"
+    >
+      <span>Save Changes</span>
+    </Button>
+  );
+};
+
+export const FormResetButton = ({
+  disabled,
+  onClick,
+}: {
+  disabled?: boolean;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+}) => {
+  return (
+    <Button
+      onClick={onClick}
+      variant="text"
+      className="normal-case"
+      startIcon={<DeleteIcon />}
+      disabled={disabled}
+      color="warning"
+    >
+      Discard Changes
+    </Button>
+  );
+};
+
+type FormButtonsProps = {
+  onClickDiscard?: React.MouseEventHandler<HTMLButtonElement>;
+  isPending: boolean;
+};
+
+export const FormButtons = ({
+  onClickDiscard,
+  isPending,
+}: FormButtonsProps) => {
+  const methods = useFormContext();
+
+  return (
+    <div className="flex flex-wrap justify-end items-center gap-2">
+      <FormResetButton
+        disabled={!methods.formState.isDirty || isPending}
+        onClick={
+          onClickDiscard ??
+          (() => {
+            methods.reset();
+          })
+        }
+      />
+      <FormSubmitButton
+        disabled={!methods.formState.isDirty}
+        loading={isPending}
+      />
+    </div>
+  );
+};
+
+export default FormButtons;
