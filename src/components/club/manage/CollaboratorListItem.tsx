006d622405297f1eb4c5d5214acc00e8ffdff270
Isosc <171582725+Isoscelestial@users.noreply.github.com>
2025-12-07 19:50:32 -0600

    Implement requested responsive CollaboratorListItem

--- src/components/club/manage/CollaboratorListItem.tsx
+++ src/components/club/manage/CollaboratorListItem.tsx
@@ -49,8 +49,8 @@ const CollaboratorListItem = withForm({
     // TODO: Insert code to fetch email by userID
 
     return (
-      <div className="flex items-center gap-2 p-2 hover:bg-slate-100 transition-colors rounded-lg">
-        <div className="grow px-4">
+      <div className="flex flex-wrap items-center gap-2 p-2 sm:hover:bg-slate-100 max-sm:bg-slate-100 transition-colors rounded-lg">
+        <div className="flex flex-col grow px-4 max-h-full min-h-10 justify-center">
           <Typography variant="body1">
             <span>{form.getFieldValue(`officers[${index}].name`)}</span>
             {self && <span>&nbsp;(You)</span>}
@@ -60,87 +60,89 @@ const CollaboratorListItem = withForm({
             <span>{userEmail}</span>
           </Typography>
         </div>
-        {form.getFieldValue(`officers[${index}].position`) == 'President' && (
-          <Typography variant="caption" className="self-center">
-            Admin
-          </Typography>
-        )}
-        <form.Field name={`officers[${index}].position`}>
-          {(subField) => (
-            <Tooltip
-              title={
-                canTogglePresident ? (
-                  self ? (
-                    <div className="text-center">
-                      You cannot take away your own admin status
-                      <br />
-                      Another admin must remove you
-                    </div>
-                  ) : subField.state.value === 'President' ? (
-                    'Make Collaborator'
-                  ) : (
-                    'Make Admin'
-                  )
-                ) : (
-                  'Only an admin can change admin status'
-                )
-              }
-            >
-              {/* This span is required to ensure the locked tooltip shows when the IconButton is disabled */}
-              <span>
-                <IconButton
-                  aria-label="change admin status"
-                  onClick={() =>
-                    subField.handleChange(
-                      subField.state.value === 'President'
-                        ? 'Officer'
-                        : 'President',
-                    )
-                  }
-                  disabled={self || !canTogglePresident}
-                >
-                  {subField.state.value === 'President' ? (
-                    <SupervisorAccountIcon
-                      color={
-                        canTogglePresident && !self ? 'primary' : undefined
-                      }
-                    />
-                  ) : (
-                    <PersonIcon />
-                  )}
-                </IconButton>
-              </span>
-            </Tooltip>
+        <div className="flex gap-2 grow justify-end">
+          {form.getFieldValue(`officers[${index}].position`) == 'President' && (
+            <Typography variant="caption" className="self-center">
+              Admin
+            </Typography>
           )}
-        </form.Field>
-        <Tooltip
-          title={
-            canRemove ? (
-              self ? (
-                <div className="text-center">
-                  You cannot remove yourself
-                  <br />
-                  Another admin must remove you
-                </div>
+          <form.Field name={`officers[${index}].position`}>
+            {(subField) => (
+              <Tooltip
+                title={
+                  canTogglePresident ? (
+                    self ? (
+                      <div className="text-center">
+                        You cannot take away your own admin status
+                        <br />
+                        Another admin must remove you
+                      </div>
+                    ) : subField.state.value === 'President' ? (
+                      'Make Collaborator'
+                    ) : (
+                      'Make Admin'
+                    )
+                  ) : (
+                    'Only an admin can change admin status'
+                  )
+                }
+              >
+                {/* This span is required to ensure the locked tooltip shows when the IconButton is disabled */}
+                <span>
+                  <IconButton
+                    aria-label="change admin status"
+                    onClick={() =>
+                      subField.handleChange(
+                        subField.state.value === 'President'
+                          ? 'Officer'
+                          : 'President',
+                      )
+                    }
+                    disabled={self || !canTogglePresident}
+                  >
+                    {subField.state.value === 'President' ? (
+                      <SupervisorAccountIcon
+                        color={
+                          canTogglePresident && !self ? 'primary' : undefined
+                        }
+                      />
+                    ) : (
+                      <PersonIcon />
+                    )}
+                  </IconButton>
+                </span>
+              </Tooltip>
+            )}
+          </form.Field>
+          <Tooltip
+            title={
+              canRemove ? (
+                self ? (
+                  <div className="text-center">
+                    You cannot remove yourself
+                    <br />
+                    Another admin must remove you
+                  </div>
+                ) : (
+                  'Remove'
+                )
               ) : (
-                'Remove'
+                'Only an admin can remove people'
               )
-            ) : (
-              'Only an admin can remove people'
-            )
-          }
-        >
-          {/* This span is required to ensure the locked tooltip shows when the IconButton is disabled */}
-          <span>
-            <IconButton
-              aria-label="remove"
-              onClick={handleRemove}
-              disabled={self || !canRemove}
-            >
-              <DeleteIcon />
-            </IconButton>
-          </span>
-        </Tooltip>
+            }
+          >
+            {/* This span is required to ensure the locked tooltip shows when the IconButton is disabled */}
+            <span>
+              <IconButton
+                aria-label="remove"
+                onClick={handleRemove}
+                disabled={self || !canRemove}
+              >
+                <DeleteIcon />
+              </IconButton>
+            </span>
+          </Tooltip>
+        </div>
       </div>
     );
   },
