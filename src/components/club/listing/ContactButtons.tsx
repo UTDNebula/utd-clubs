d13ceefde933dd249bed1f3700e7bdd0bb72bdb8
Tyler Hill <gotyhill@gmail.com>
2025-12-09 16:59:47 -0600

    MUI icons and better brand colors for contacts

--- src/components/club/listing/ContactButtons.tsx
+++ src/components/club/listing/ContactButtons.tsx
@@ -1,41 +1,30 @@
-import Tooltip from '@mui/material/Tooltip';
+import { IconButton, Tooltip } from '@mui/material';
 import Link from 'next/link';
 import { logo } from '@src/icons/ContactIcons';
-import type { SelectContact as Contacts } from '@src/server/db/models';
+import type { SelectContact } from '@src/server/db/models';
 import { contactNames } from '@src/server/db/schema/contacts';
 
-const EmailButton = ({ item }: { item: Contacts }) => {
-  return (
-    <button className="group relative h-min self-center rounded-full bg-slate-100 p-2.5 transition-colors hover:color-red-100">
-      <Link href={`mailto:${item.url}`}>
-        <div className="relative h-8 w-8">{logo[item.platform]}</div>
-      </Link>
-    </button>
-  );
+type ContactButtonProps = {
+  contacts: SelectContact[];
 };
-
-type contentButtonProps = {
-  contacts: Array<Contacts>;
-};
-const ContactButtons = ({ contacts }: contentButtonProps) => {
+const ContactButtons = ({ contacts }: ContactButtonProps) => {
   return (
     <div className="flex flex-wrap gap-4">
       {contacts.map((item) => (
-        <div key={item.platform + item.url}>
-          {item.platform === 'email' ? (
-            <Tooltip title="Email">
-              <EmailButton item={item} />
-            </Tooltip>
-          ) : (
-            <Tooltip title={contactNames[item.platform]}>
-              <button className="group relative h-min self-center rounded-full bg-slate-100 p-2.5 transition-colors">
-                <Link href={item.url} target="_blank">
-                  <div className="relative h-8 w-8">{logo[item.platform]}</div>
-                </Link>
-              </button>
-            </Tooltip>
-          )}
-        </div>
+        <Tooltip
+          key={item.platform + item.url}
+          title={contactNames[item.platform]}
+        >
+          <Link
+            href={item.platform === 'email' ? `mailto:${item.url}` : item.url}
+            target="_blank"
+            className="inline-block"
+          >
+            <IconButton className="group bg-slate-100" size="large">
+              {logo[item.platform]}
+            </IconButton>
+          </Link>
+        </Tooltip>
       ))}
     </div>
   );
