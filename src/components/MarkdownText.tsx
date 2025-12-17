5f404910f572cd1447481b9933de931a9b6e2a32
Tyler Hill <gotyhill@gmail.com>
2025-12-17 15:35:00 -0600

    Remove HTML

--- src/components/MarkdownText.tsx
+++ src/components/MarkdownText.tsx
@@ -1,12 +1,13 @@
 import Markdown from 'react-markdown';
 import rehypeExternalLinks from 'rehype-external-links';
 import remarkGfm from 'remark-gfm';
+import remarkStripHtml from 'remark-strip-html';
 
 const MarkdownText = ({ text }: { text: string }) => {
   return (
     <div className="prose prose-slate prose-a:text-royal prose-ul:[&.contains-task-list]:list-none prose-ul:[&.contains-task-list]:pl-2">
       <Markdown
-        remarkPlugins={[remarkGfm]}
+        remarkPlugins={[remarkStripHtml, remarkGfm]}
         rehypePlugins={[
           [rehypeExternalLinks, { rel: ['noreferrer'], target: ['_blank'] }],
         ]}
