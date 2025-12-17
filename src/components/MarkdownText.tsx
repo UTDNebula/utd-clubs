cdd103ec13d2fd1321b59059f924caa84ef1ae89
Tyler Hill <gotyhill@gmail.com>
2025-12-17 15:46:57 -0600

    Switch to rehype-sanitize

--- src/components/MarkdownText.tsx
+++ src/components/MarkdownText.tsx
@@ -1,15 +1,16 @@
 import Markdown from 'react-markdown';
 import rehypeExternalLinks from 'rehype-external-links';
+import rehypeSanitize from 'rehype-sanitize';
 import remarkGfm from 'remark-gfm';
-import remarkStripHtml from 'remark-strip-html';
 
 const MarkdownText = ({ text }: { text: string }) => {
   return (
     <div className="prose prose-slate prose-a:text-royal prose-ul:[&.contains-task-list]:list-none prose-ul:[&.contains-task-list]:pl-2">
       <Markdown
-        remarkPlugins={[remarkStripHtml, remarkGfm]}
+        remarkPlugins={[remarkGfm]}
         rehypePlugins={[
           [rehypeExternalLinks, { rel: ['noreferrer'], target: ['_blank'] }],
+          rehypeSanitize,
         ]}
       >
         {text}
