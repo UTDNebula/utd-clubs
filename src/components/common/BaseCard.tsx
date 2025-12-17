7535ad3722b876351ee863adb4f1e82c79b043fc
sy-pk <173501972+sy-pk@users.noreply.github.com>
2025-12-16 21:15:21 -0600

    feat: add BaseCard component

---
+++ src/components/common/BaseCard.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+type CardVariant = 'flat' | 'interactive';
+
+type BaseCardProps = {
+  children: React.ReactNode;
+  variant?: CardVariant;
+  className?: string;
+};
+
+const baseClasses = 'rounded-lg bg-white';
+
+const variantClasses: Record<CardVariant, string> = {
+  flat: 'shadow-none',
+  interactive: 'shadow-lg transition-shadow hover:shadow-2xl',
+};
+
+export const BaseCard = ({
+  children,
+  variant = 'flat',
+  className = '',
+}: BaseCardProps) => {
+  return (
+    <div
+      className={`${baseClasses} ${variantClasses[variant]} ${className}`}
+    >
+      {children}
+    </div>
+  );
+};
