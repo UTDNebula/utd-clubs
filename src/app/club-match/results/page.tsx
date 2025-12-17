80af4c8bf1c467c86451f3c3cca432e0836ec325
sy-pk <173501972+sy-pk@users.noreply.github.com>
2025-12-16 23:11:17 -0600

    refactor: apply BaseCard to ClubMatch results

--- src/app/club-match/results/page.tsx
+++ src/app/club-match/results/page.tsx
@@ -4,6 +4,7 @@ import { headers } from 'next/headers';
 import Link from 'next/link';
 import { redirect } from 'next/navigation';
 import JoinButton from '@src/components/club/JoinButton';
+import { BaseCard } from '@src/components/common/BaseCard';
 import Header from '@src/components/header/BaseHeader';
 import { auth } from '@src/server/auth';
 import { db } from '@src/server/db';
@@ -48,31 +49,33 @@ const Page = async () => {
         </h1>
         <div className="grid w-full auto-rows-fr grid-cols-[repeat(auto-fill,320px)] justify-center gap-16 pb-4">
           {data.clubMatch.map((club) => (
-            <Link
+            <BaseCard
               key={club.id}
-              href={'/directory/' + club.id}
-              className="flex flex-col gap-2 rounded-lg bg-white shadow-2xl p-6"
+              variant="interactive"
+              className="flex flex-col gap-2 p-6"
             >
-              <p className="line-clamp-2 text-2xl font-medium text-slate-800 md:text-xl">
-                {club.name}
-              </p>
-              <p className="text-base text-slate-600 md:text-sm">
-                {club.reasoning}
-              </p>
-              <ul>
-                {club.benefit.split(', ').map((benefit) => (
-                  <li
-                    key={benefit}
-                    className="ml-6 list-disc text-base text-slate-600 md:text-sm"
-                  >
-                    {benefit.charAt(0).toUpperCase() + benefit.slice(1)}
-                  </li>
-                ))}
-              </ul>
-              <div className="mt-auto flex flex-row space-x-2">
-                <JoinButton clubId={club.id} />
-              </div>
-            </Link>
+              <Link href={'/directory/' + club.id}>
+                <p className="line-clamp-2 text-2xl font-medium text-slate-800 md:text-xl">
+                  {club.name}
+                </p>
+                <p className="text-base text-slate-600 md:text-sm">
+                  {club.reasoning}
+                </p>
+                <ul>
+                  {club.benefit.split(', ').map((benefit) => (
+                    <li
+                      key={benefit}
+                      className="ml-6 list-disc text-base text-slate-600 md:text-sm"
+                    >
+                      {benefit.charAt(0).toUpperCase() + benefit.slice(1)}
+                    </li>
+                  ))}
+                </ul>
+                <div className="mt-auto flex flex-row space-x-2">
+                  <JoinButton clubId={club.id} />
+                </div>
+              </Link>
+            </BaseCard>
           ))}
         </div>
         {(data.clubMatchLimit == null || data.clubMatchLimit > 0) && (
