9439c6d0fcdf6c134f47809876951497ffe6c59a
AbhiramTadepalli <abhiram.tadepalli.7@gmail.com>
2025-12-19 13:22:51 -0600

    remove text on header

--- src/components/club/listing/ClubHeader.tsx
+++ src/components/club/listing/ClubHeader.tsx
@@ -3,7 +3,6 @@ import type {
   SelectContact as Contacts,
   SelectClub,
 } from '@src/server/db/models';
-import JoinButton from '../JoinButton';
 
 type Club = SelectClub & {
   contacts?: Contacts[];
@@ -19,18 +18,6 @@ const ClubHeader = async ({ club }: { club: Club }) => {
         className="object-cover object-center"
         priority
       />
-      <div className="absolute inset-0 z-10 flex flex-wrap items-center p-4 md:px-20 gap-4">
-        <h1
-          className={`font-display font-bold text-slate-100 text-shadow-[0_0_16px_rgb(0_0_0_/_0.4)] ${
-            club.name.length > 10 ? 'text-3xl' : 'text-5xl'
-          }`}
-        >
-          {club.name}
-        </h1>
-        <div className="ml-auto flex items-center gap-x-6">
-          <JoinButton isHeader clubId={club.id} clubSlug={club.slug} />
-        </div>
-      </div>
     </div>
   );
 };
