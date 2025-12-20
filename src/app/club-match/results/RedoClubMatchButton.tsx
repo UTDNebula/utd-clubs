1bd798c095393d404635b8a9a9d62d606298cc56
AbhiramTadepalli <abhiram.tadepalli.7@gmail.com>
2025-12-20 10:10:24 -0600

    don't router.push, just href it

--- src/app/club-match/results/RedoClubMatchButton.tsx
+++ src/app/club-match/results/RedoClubMatchButton.tsx
@@ -1,22 +1,16 @@
 'use client';
 
 import { Button, CircularProgress } from '@mui/material';
-import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 
 const RedoClubMatchButton = () => {
-  const router = useRouter();
   const [loading, setLoading] = useState(false);
-
-  const handleClick = () => {
-    setLoading(true);
-    router.push('/club-match');
-  };
 
   return (
     <Button
       variant="contained"
-      onClick={handleClick}
+      onClick={() => setLoading(true)}
+      href="/club-match"
       loading={loading}
       loadingPosition="start"
       className="rounded-full w-fit normal-case self-center"
