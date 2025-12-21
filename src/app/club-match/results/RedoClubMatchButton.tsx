0ca8b8208e92da1db59917d774ae7f35eaf6d5d3
Tyler Hill <gotyhill@gmail.com>
2025-12-20 20:48:01 -0600

    Revert "Remove loading state on Redo button"
    This reverts commit 927524b49495cebdcdca54336b4d9786318abd9f.

--- src/app/club-match/results/RedoClubMatchButton.tsx
+++ src/app/club-match/results/RedoClubMatchButton.tsx
@@ -2,18 +2,22 @@
 
 import { RestartAlt } from '@mui/icons-material';
 import { Button } from '@mui/material';
-import Link from 'next/link';
+import { useState } from 'react';
 
 const RedoClubMatchButton = () => {
+  const [loading, setLoading] = useState(false);
+
   return (
     <Button
       variant="contained"
-      component={Link}
+      onClick={() => setLoading(true)}
       href="/club-match"
+      loading={loading}
+      loadingPosition="start"
       className="rounded-full w-fit normal-case self-center"
       startIcon={<RestartAlt />}
     >
-      Redo Club Match
+      {loading ? 'Loading...' : 'Redo Club Match'}
     </Button>
   );
 };
