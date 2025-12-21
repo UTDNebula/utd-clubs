927524b49495cebdcdca54336b4d9786318abd9f
Tyler Hill <gotyhill@gmail.com>
2025-12-20 20:45:13 -0600

    Remove loading state on Redo button

--- src/app/club-match/results/RedoClubMatchButton.tsx
+++ src/app/club-match/results/RedoClubMatchButton.tsx
@@ -2,22 +2,18 @@
 
 import { RestartAlt } from '@mui/icons-material';
 import { Button } from '@mui/material';
-import { useState } from 'react';
+import Link from 'next/link';
 
 const RedoClubMatchButton = () => {
-  const [loading, setLoading] = useState(false);
-
   return (
     <Button
       variant="contained"
-      onClick={() => setLoading(true)}
+      component={Link}
       href="/club-match"
-      loading={loading}
-      loadingPosition="start"
       className="rounded-full w-fit normal-case self-center"
       startIcon={<RestartAlt />}
     >
-      {loading ? 'Loading...' : 'Redo Club Match'}
+      Redo Club Match
     </Button>
   );
 };
