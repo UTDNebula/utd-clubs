6b3688e035cbfd2c05cf78ac68b86dab2191666c
AbhiramTadepalli <abhiram.tadepalli.7@gmail.com>
2025-12-22 07:48:56 -0600

    BaseCard: transition-all and allow id

--- src/components/common/BaseCard.tsx
+++ src/components/common/BaseCard.tsx
@@ -4,23 +4,26 @@ type BaseCardProps = {
   children: React.ReactNode;
   variant?: CardVariant;
   className?: string;
+  id?: string;
 };
 
 const baseClasses = 'rounded-lg';
 
 const variantClasses: Record<CardVariant, string> = {
   flat: 'shadow-none',
-  interactive: 'shadow-lg transition-shadow hover:shadow-xl',
+  interactive: 'shadow-lg transition-all hover:shadow-xl',
 };
 
 export const BaseCard = ({
   children,
   variant = 'flat',
   className = '',
+  id = '',
 }: BaseCardProps) => {
   const hasBgClass = className.includes('bg-');
   return (
     <div
+      id={id}
       className={`${baseClasses} ${hasBgClass ? '' : 'bg-white'} ${variantClasses[variant]} ${className}`}
     >
       {children}
