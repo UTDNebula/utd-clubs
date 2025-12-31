fee46b538696ba56b383497cf9d5e8cc0b7f42af
AbhiramTadepalli <abhiram.tadepalli.7@gmail.com>
2025-12-31 08:21:06 -0600

    add key prop to ContactButtons inside map

--- src/components/club/listing/ClubContactCard.tsx
+++ src/components/club/listing/ClubContactCard.tsx
@@ -11,7 +11,9 @@ export default function ClubContactCard({ club }: ClubContactCardProps) {
     <BaseCard className="flex flex-col bg-neutral-50 shadow-sm p-5 gap-2">
       <h2 className="text-xl font-bold text-slate-900 mb-2">Contact</h2>
       {club.contacts && club.contacts.length > 0 ? (
-        club.contacts.map((contact) => <ContactButton contact={contact} />)
+        club.contacts.map((contact) => (
+          <ContactButton key={contact.platform} contact={contact} />
+        ))
       ) : (
         <span className="text-slate-500 text-sm">No contact info</span>
       )}
