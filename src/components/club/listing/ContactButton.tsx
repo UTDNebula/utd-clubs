8d984af2ef29fe66e73e921496db953a7ca415fd
AbhiramTadepalli <abhiram.tadepalli.7@gmail.com>
2026-01-02 11:55:37 -0600

    isolate usernames from contact.url's

--- src/components/club/listing/ContactButton.tsx
+++ src/components/club/listing/ContactButton.tsx
@@ -4,6 +4,21 @@ import { logo } from '@src/icons/ContactIcons';
 import type { SelectContact } from '@src/server/db/models';
 import { contactNames } from '@src/server/db/schema/contacts';
 
+function contactDisplay(contact: SelectContact) {
+  switch (contact.platform) {
+    case "discord":   return contact.url.split('://')[1]?.replace("www.", "").replace(/\/+$/, "") ?? contact.platform
+    case "instagram": return contact.url.indexOf("instagram.com/") >= 0 ? "@" + contact.url.split("instagram.com/")[1]!.replace(/\/+$/, "") : contact.platform;
+    case "website":   return contact.url.replace("http://", "").replace("https://", "").replace("www.", "").replace(/\/+$/, "");
+    case "email":     return contact.url
+    case "twitter":   return contact.url.indexOf(".com/") > 0 ? "@" + contact.url.split(".com/")[1]!.replace(/\/+$/, "") : contact.platform;
+    case "facebook":  return contact.url.indexOf(".com/") > 0 ? "@" + contact.url.split(".com/")[1]!.replace(/\/+$/, "") : contact.platform;
+    case "youtube":   return contact.url.indexOf("youtube.com/@") >= 0 ? contact.url.split("youtube.com/")[1]!.replace(/\/+$/, "") : contact.platform;
+    case "twitch":    return contact.url.split("twitch.tv/")[1]?.replace(/\/+$/, "") ?? contact.platform;
+    case "linkedIn":  return contact.platform;
+    default: return contact.platform;
+  }
+}
+
 type ContactButtonProps = {
   contact: SelectContact;
 };
@@ -21,17 +36,12 @@ const ContactButton = ({ contact }: ContactButtonProps) => {
         className="inline-block w-full bg-white shadow-sm rounded-4xl hover:bg-gray-100 transition-colors duration-200"
       >
         <IconButton
-          className="group"
+          className="group hover:bg-inherit"
           size="medium"
-          sx={{
-            '&:hover': {
-              backgroundColor: 'inherit',
-            },
-          }}
         >
           {logo[contact.platform]}
         </IconButton>
-        <span>{contact.platform}</span>
+        <span>{contactDisplay(contact)}</span>
       </Link>
     </Tooltip>
   );
