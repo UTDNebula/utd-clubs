ebcdcda933f703ebc93b0913f9676d84f9f88e05
Tyler Hill <gotyhill@gmail.com>
2026-01-11 01:25:05 -0600

    Link color fix

--- src/components/MarkdownText.tsx
+++ src/components/MarkdownText.tsx
@@ -5,7 +5,7 @@ import remarkGfm from 'remark-gfm';
 
 const MarkdownText = ({ text }: { text: string }) => {
   return (
-    <div className="prose prose-slate dark:prose-invert prose-a:text-royal prose-ul:[&.contains-task-list]:list-none prose-ul:[&.contains-task-list]:pl-2">
+    <div className="prose prose-slate dark:prose-invert prose-a:text-royal dark:prose-a:text-cornflower-300 prose-ul:[&.contains-task-list]:list-none prose-ul:[&.contains-task-list]:pl-2">
       <Markdown
         remarkPlugins={[remarkGfm]}
         rehypePlugins={[
