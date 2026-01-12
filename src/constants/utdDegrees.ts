26cdee4722b5354a7b8632b0d1fcffe6dc7f5dad
Isosc <171582725+Isoscelestial@users.noreply.github.com>
2026-01-12 03:18:59 -0600

    Remove duplicate Arts Management and Leadership

--- src/constants/utdDegrees.ts
+++ src/constants/utdDegrees.ts
@@ -123,7 +123,6 @@ export const minors = [
   'Neuroscience',
   'Psychology',
   'Speech, Language, and Hearing Sciences',
-  'Arts Management and Leadership',
   'Criminology',
   'Economics',
   'Geography',
