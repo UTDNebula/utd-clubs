1942c4f666d77d4cce4d7a42f0a0d2b7cbc0870f
AbhiramTadepalli <abhiram.tadepalli.7@gmail.com>
2026-01-13 19:42:35 -0600

    lint format

--- src/modules/markdown.ts
+++ src/modules/markdown.ts
@@ -1,5 +1,6 @@
-export function convertMarkdownToPlaintext(md : string) {
-  return md.replace(/^#+.*$/gm, '') // replace headers
+export function convertMarkdownToPlaintext(md: string) {
+  return md
+    .replace(/^#+.*$/gm, '') // replace headers
     .replace(/([^.!?])\s*\n+/g, '$1. ') // add periods to end of paragraphs and flatten
     .replace(/\n+/g, ' ') // flatten paragraphs
     .replace(/[*`_]/g, '') // remove text styling
