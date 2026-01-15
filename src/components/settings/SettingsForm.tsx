ab8ed59ce8e2f8d9d30d33903383cf0632a7f94f
Isosc <171582725+Isoscelestial@users.noreply.github.com>
2026-01-14 20:35:09 -0600

    Improved account settings API calling and error handling

--- src/components/settings/SettingsForm.tsx
+++ src/components/settings/SettingsForm.tsx
@@ -1,9 +1,12 @@
 'use server';
 
-import { eq } from 'drizzle-orm';
+import Alert from '@mui/material/Alert';
 import { auth } from '@src/server/auth';
-import { db } from '@src/server/db';
-import { userMetadata } from '@src/server/db/schema/users';
+import {
+  SelectUserMetadataToClubsWithClub,
+  SelectUserMetadataWithClubs,
+} from '@src/server/db/models';
+import { api } from '@src/trpc/server';
 import DeleteAccount from './forms/DeleteAccount';
 import JoinedClubs from './forms/JoinedClubs';
 import UserInfo from './forms/UserInfo';
@@ -16,21 +19,42 @@ async function SettingsForm({
 }) {
   const user = session.user;
 
-  const userData = await db.query.userMetadata.findFirst({
-    where: eq(userMetadata.id, user.id),
-    with: { clubs: true },
-  });
-  if (!userData) return null;
-  const joinedClubs = await db.query.userMetadataToClubs.findMany({
-    where: (joinTable) => eq(joinTable.userId, user.id),
-    with: { club: true },
+  let userData: SelectUserMetadataWithClubs | undefined = undefined;
+  let joinedClubs: SelectUserMetadataToClubsWithClub[] | undefined = undefined;
+
+  // Concurrently run both procedures
+  await Promise.allSettled([
+    api.userMetadata.byId({ id: user.id }),
+    api.club.getMemberClubsMetadata(),
+  ]).then(([userDataResult, joinedClubsResult]) => {
+    console.log('then');
+    if (userDataResult.status === 'fulfilled' && userDataResult.value) {
+      userData = userDataResult.value;
+    } else if (userDataResult.status === 'rejected') {
+      throw new Error(
+        `Failed to fetch user data. Has the \`user_metadata\` table been migrated?\n\n${userDataResult.reason}`,
+      );
+    }
+    if (joinedClubsResult.status === 'fulfilled' && joinedClubsResult.value) {
+      joinedClubs = joinedClubsResult.value;
+    } else if (joinedClubsResult.status === 'rejected') {
+      throw new Error(
+        `Failed to fetch joined clubs. Has the \`user_metadata_to_clubs\` table been migrated?\n\n${joinedClubsResult.reason}`,
+      );
+    }
   });
 
   return (
     <div className="flex flex-col gap-8 w-full max-w-6xl">
+      {(!userData || !joinedClubs) && (
+        <Alert severity="error" variant="filled" className="rounded-lg">
+          One or more panels were hidden because their associated data could not
+          be found.
+        </Alert>
+      )}
       <SettingsHeader user={user} />
-      <UserInfo user={userData} />
-      <JoinedClubs joinedClubs={joinedClubs} />
+      {userData && <UserInfo user={userData} />}
+      {joinedClubs && <JoinedClubs joinedClubs={joinedClubs} />}
       <DeleteAccount />
     </div>
   );
