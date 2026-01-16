d81a137c027eea444c4c60a9eaa2a7262d917b15
Tyler Hill <gotyhill@gmail.com>
2026-01-16 16:48:09 -0600

    Auth page

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -27,12 +27,13 @@ export default async function Auth(props: {
             className="select-none"
           />
         </div>
+        <div className="fixed inset-0 dark:bg-slightly-darken" />
         <div className="z-10">
           <Link
             href="/"
             className="lext-lg md:text-xl font-display font-medium md:font-bold flex gap-2 items-center text-white text-shadow-[0_0_4px_rgb(0_0_0_/_0.4)]"
           >
-            <NebulaLogo className="h-6 w-auto fill-white dark:fill-haiti drop-shadow-[0_0_4px_rgb(0_0_0_/_0.4)]" />
+            <NebulaLogo className="h-6 w-auto fill-white drop-shadow-[0_0_4px_rgb(0_0_0_/_0.4)]" />
             UTD CLUBS
           </Link>
         </div>
