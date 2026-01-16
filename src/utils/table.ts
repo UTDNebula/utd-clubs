b4dda30db8ffeb2c8f6f6a0ec432deda12afacff
AbhiramTadepalli <abhiram.tadepalli.7@gmail.com>
2026-01-15 19:56:28 -0600

    use alias in name filtering for admin search

--- src/utils/table.ts
+++ src/utils/table.ts
@@ -1,11 +1,17 @@
-import { rankItem } from '@tanstack/match-sorter-utils';
+import { rankings, rankItem } from '@tanstack/match-sorter-utils';
 import { type FilterFn } from '@tanstack/react-table';
 import { RouterOutputs } from '@src/trpc/shared';
 
 export const fuzzyFilter: FilterFn<
   RouterOutputs['admin']['allClubs'][number]
 > = (row, columnId, value, addMeta) => {
-  const itemRank = rankItem(row.getValue(columnId), value as string);
+  const nameRank = rankItem(row.getValue(columnId), value as string);
+  const aliasRank = rankItem(row.original.alias ?? '', value as string);
+  const itemRank = {
+    ...nameRank,
+    passed: nameRank.passed || aliasRank.passed,
+    rank: nameRank.rank + 2 * aliasRank.rank,
+  };
   addMeta({ itemRank });
   return itemRank.passed;
 };
