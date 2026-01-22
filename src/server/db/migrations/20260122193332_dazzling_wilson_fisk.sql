34ac33baa9bee3dd6d8527755cbcfd83dc8dadb4
Ethan Bickel <notethanbickel@gmail.com>
2026-01-22 14:03:08 -0600

    fix: if columns/types exist clauses

--- src/server/db/migrations/20260122193332_dazzling_wilson_fisk.sql
+++ src/server/db/migrations/20260122193332_dazzling_wilson_fisk.sql
@@ -1,17 +1,18 @@
+DROP TYPE IF EXISTS "public"."student_classification" CASCADE;--> statement-breakpoint
 CREATE TYPE "public"."student_classification" AS ENUM('Student', 'Graduate Student', 'Alum', 'Prospective Student');--> statement-breakpoint
 ALTER TYPE "public"."approved_enum" ADD VALUE 'deleted';--> statement-breakpoint
 DROP MATERIALIZED VIEW "public"."used_tags";--> statement-breakpoint
 DROP INDEX "club_search_idx";--> statement-breakpoint
-ALTER TABLE "club" ADD COLUMN "alias" text;--> statement-breakpoint
-ALTER TABLE "contacts" ADD COLUMN "display_order" integer;--> statement-breakpoint
-ALTER TABLE "officers" ADD COLUMN "display_order" integer;--> statement-breakpoint
-ALTER TABLE "user_metadata" ADD COLUMN "student_classification" "student_classification" DEFAULT 'Student' NOT NULL;--> statement-breakpoint
-ALTER TABLE "user_metadata" ADD COLUMN "graduation_date" date;--> statement-breakpoint
-ALTER TABLE "user_metadata" ADD COLUMN "contact_email" text;--> statement-breakpoint
+ALTER TABLE "club" ADD COLUMN IF NOT EXISTS "alias" text;--> statement-breakpoint
+ALTER TABLE "contacts" ADD COLUMN IF NOT EXISTS "display_order" integer;--> statement-breakpoint
+ALTER TABLE "officers" ADD COLUMN IF NOT EXISTS "display_order" integer;--> statement-breakpoint
+ALTER TABLE "user_metadata" ADD COLUMN IF NOT EXISTS "student_classification" "student_classification" DEFAULT 'Student' NOT NULL;--> statement-breakpoint
+ALTER TABLE "user_metadata" ADD COLUMN IF NOT EXISTS "graduation_date" date;--> statement-breakpoint
+ALTER TABLE "user_metadata" ADD COLUMN IF NOT EXISTS "contact_email" text;--> statement-breakpoint
 CREATE INDEX "club_search_idx" ON "club" USING bm25 ("id","name","alias","description","tags","approved") WITH (key_field=id,text_fields='{"tags":{"tokenizer":{"type":"keyword"}},"name":{"tokenizer":{"type":"default","stemmer":"English"}}}',numeric_fields='{"approved":{"fast":true}}');--> statement-breakpoint
-ALTER TABLE "user_metadata" DROP COLUMN "year";--> statement-breakpoint
-ALTER TABLE "user_metadata" DROP COLUMN "role";--> statement-breakpoint
-ALTER TABLE "user_metadata" DROP COLUMN "career";--> statement-breakpoint
+ALTER TABLE "user_metadata" DROP COLUMN IF EXISTS "year";--> statement-breakpoint
+ALTER TABLE "user_metadata" DROP COLUMN IF EXISTS "role";--> statement-breakpoint
+ALTER TABLE "user_metadata" DROP COLUMN IF EXISTS "career";--> statement-breakpoint
 CREATE MATERIALIZED VIEW "public"."used_tags" AS (
   SELECT 
     tag, 
