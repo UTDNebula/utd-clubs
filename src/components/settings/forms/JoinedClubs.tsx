98b43dc001ac5e34ddf383f77409a980b46776d9
jqhz <jasonhuang3030@gmail.com>
2026-02-15 07:54:32 -0600

    removed unnecessary div wrapper

--- src/components/settings/forms/JoinedClubs.tsx
+++ src/components/settings/forms/JoinedClubs.tsx
@@ -35,15 +35,14 @@ export default function JoinedClubs({ joinedClubs }: ClubsProps) {
     <Panel heading="Followed Clubs">
       {joinedClubs.length > 0 ? (
         joinedClubs.map((joinedClub) => (
-          <div key={joinedClub.club.id} className="shrink-0">
-            <ClubListItem
-              joinedClub={joinedClub}
-              onLeave={() => {
-                setLeaveClub(joinedClub);
-                setOpenLeaveModal(true);
-              }}
-            />
-          </div>
+          <ClubListItem
+            joinedClub={joinedClub}
+            key={joinedClub.club.id}
+            onLeave={() => {
+              setLeaveClub(joinedClub);
+              setOpenLeaveModal(true);
+            }}
+          />
         ))
       ) : (
         <div className="w-full py-12 flex flex-col items-center justify-center border-2 border-dashed border-slate-200 dark:border-slate-800 rounded-xl text-md font-medium text-slate-600 dark:text-slate-400">
