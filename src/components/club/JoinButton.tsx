1ca005f3b908cfaf34381425f818af3c039ca020
Ali <abenrami06@gmail.com>
2026-02-24 13:46:23 -0600

    capitalize snackbar messages for club join/leave action

--- src/components/club/JoinButton.tsx
+++ src/components/club/JoinButton.tsx
@@ -62,7 +62,7 @@ const JoinButton = ({ isHeader, clubId, clubSlug }: JoinButtonProps) => {
         const joined = context?.memberType === undefined;
 
         setSnackbar({
-          message: joined ? 'followed club!' : 'Left club!',
+          message: joined ? 'Followed club!' : 'Left club!',
           type: joined ? 'success' : 'info',
           autoHideDuration: true,
           fitContent: true,
