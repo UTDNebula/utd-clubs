a1004cfd535793f555e1197392f445c4a513031b
sy-pk <173501972+sy-pk@users.noreply.github.com>
2026-03-28 22:37:18 -0500

    feat: replace Syncfusion default styles with custom Theme Studio theme

--- src/app/community/calendar/layout.tsx
+++ src/app/community/calendar/layout.tsx
@@ -1,4 +1,6 @@
-import '@syncfusion/ej2-react-schedule/styles/material.css';
+import '@src/styles/syncfusion/material.css';
+import '@src/styles/syncfusion/overrides.css';
+
 import type { ReactNode } from 'react';
 import SyncfusionWrapper from '@src/lib/SyncfusionWrapper';
 
