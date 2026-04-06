de2f0f9c9d838504dd47fee7f7cf0537b17c425e
Isosc <171582725+Isoscelestial@users.noreply.github.com>
2026-04-05 20:03:08 -0500

    feat(events): add snackbar when using a filter that requires being signed in

--- src/components/events/directory/filter/panels/Filters.tsx
+++ src/components/events/directory/filter/panels/Filters.tsx
@@ -1,9 +1,13 @@
+import Button from '@mui/material/Button';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Switch from '@mui/material/Switch';
 import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import { memo } from 'react';
 import Panel from '@src/components/common/Panel';
+import { useRegisterModal } from '@src/components/global/RegisterModalProvider';
+import { setSnackbar } from '@src/components/global/Snackbar';
+import { authClient } from '@src/utils/auth-client';
 import { EventFiltersSchema } from '@src/utils/eventFilter';
 import { FilterPanelProps, panelProps, setEventsParams } from '../utils';
 
@@ -15,6 +19,31 @@ export type FiltersPanelFields = Pick<
 export default memo(function FiltersPanel(
   props: FilterPanelProps<FiltersPanelFields>,
 ) {
+  const session = authClient.useSession();
+  const signedIn = Boolean(session.data);
+
+  const { setShowRegisterModal } = useRegisterModal();
+
+  function showSignInMessage() {
+    setSnackbar({
+      message: "This filter option only works when you're signed in",
+      autoHideDuration: true,
+      fitContent: true,
+      closeOn: ['timeout', 'escapeKeyDown', 'dismiss'],
+      action: (
+        <Button
+          size="small"
+          onClick={() => {
+            setShowRegisterModal(true);
+          }}
+          color="inherit"
+        >
+          Sign in
+        </Button>
+      ),
+    });
+  }
+
   const clubs = props.filters.clubs;
   const hideRegistered = props.filters.hideRegistered;
   const past = props.filters.past;
@@ -26,6 +55,7 @@ export default memo(function FiltersPanel(
         exclusive
         onChange={(_e, newValue) => {
           if (newValue !== null) {
+            if (!signedIn) showSignInMessage();
             setEventsParams((params) => {
               if (newValue !== 'all') {
                 params.set('clubs', newValue);
@@ -49,6 +79,7 @@ export default memo(function FiltersPanel(
           <Switch
             checked={hideRegistered}
             onChange={(_e, newValue) => {
+              if (!signedIn) showSignInMessage();
               setEventsParams((params) => {
                 if (newValue) {
                   params.set('hideRegistered', '');
