96e698403b5527f9ca049df0e30125b36581c957
T0MT0P <mixels572@gmail.com>
2026-04-21 18:00:58 -0500

    changed "There are no upcoming events" to "This club has not posted info about upcoming events"

--- src/components/club/listing/ClubBody.tsx
+++ src/components/club/listing/ClubBody.tsx
@@ -64,7 +64,7 @@ const ClubBody = async ({
           emptyText={
             club.updatedAt == null || club.updatedAt < oneYearAgo
               ? 'No info about upcoming events'
-              : 'There are no upcoming events'
+              : 'This club has not posted info about upcoming events'
           }
         />
       </div>
