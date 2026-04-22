8337db0a524d1d039f97231fbd06695cdc3d09de
sy-pk <173501972+sy-pk@users.noreply.github.com>
2026-04-22 16:15:14 -0500

    feat: connect EventCalendar to events directory calendar tab

--- src/app/events/(directory)/calendar/page.tsx
+++ src/app/events/(directory)/calendar/page.tsx
@@ -1,14 +1,5 @@
-const Events = async () => {
-  return (
-    <main className="font-bold relative select-none h-72">
-      <h1 className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-[200px] text-slate-200 dark:text-slate-800">
-        WIP
-      </h1>
-      <h2 className="absolute bottom-1/2 left-1/2 -translate-x-1/2 translate-y-20 text-3xl text-slate-800 dark:text-slate-200">
-        Coming soon!
-      </h2>
-    </main>
-  );
-};
+import EventCalendar from '@src/components/community/EventCalendar';
 
-export default Events;
+export default function CalendarPage() {
+  return <EventCalendar />;
+}
