fdf97b27ac2a9638fc75ad352851d424647ac9bd
Justin <justinschwerdtfeger245@gmail.com>
2026-08-21 10:22:17 -0500

    Remove Compact from all pages

--- src/systems/clubs/match/steps/InterestsStep.tsx
+++ src/systems/clubs/match/steps/InterestsStep.tsx
@@ -41,10 +41,7 @@ const InterestsStep = withForm({
   render: function Render({ form }) {
     return (
       <div className="flex flex-col gap-4">
-        <form.Question
-          question="What types of organizations are you interested in?"
-          density="compact"
-        >
+        <form.Question question="What types of organizations are you interested in?">
           <form.AppField name="interests.categories">
             {(field) => (
               <field.MultiSelect
@@ -77,10 +74,7 @@ const InterestsStep = withForm({
           }}
         </form.Subscribe>
 
-        <form.Question
-          question="What are your hobbies or areas of interest?"
-          density="compact"
-        >
+        <form.Question question="What are your hobbies or areas of interest?">
           <form.AppField name="interests.hobbies">
             {(field) => (
               <field.MultiSelect
@@ -92,28 +86,19 @@ const InterestsStep = withForm({
           </form.AppField>
         </form.Question>
 
-        <form.Question
-          question="Please be specific about your selected hobbies."
-          density="compact"
-        >
+        <form.Question question="Please be specific about your selected hobbies.">
           <form.AppField name="interests.hobbyDetails">
             {(field) => <field.TextField className="w-full" />}
           </form.AppField>
         </form.Question>
 
-        <form.Question
-          question="Beyond your major, are there other academic topics or tracks you're interested in?"
-          density="compact"
-        >
+        <form.Question question="Beyond your major, are there other academic topics or tracks you're interested in?">
           <form.AppField name="interests.otherAcademicInterests">
             {(field) => <field.TextField className="w-full" />}
           </form.AppField>
         </form.Question>
 
-        <form.Question
-          question="What new experiences, hobbies, or activities would you be interested in?"
-          density="compact"
-        >
+        <form.Question question="What new experiences, hobbies, or activities would you be interested in?">
           <form.AppField name="interests.newExperiences">
             {(field) => <field.TextField className="w-full" />}
           </form.AppField>
--- src/systems/clubs/match/steps/InvolvementStep.tsx
+++ src/systems/clubs/match/steps/InvolvementStep.tsx
@@ -49,7 +49,7 @@ const InvolvementStep = withForm({
   render: function Render({ form }) {
     return (
       <div className="flex flex-col gap-4">
-        <form.Question question="Goals for Getting Involved" density="compact">
+        <form.Question question="Goals for Getting Involved">
           <form.AppField name="involvement.involvementGoals">
             {(field) => (
               <field.MultiSelect
@@ -60,10 +60,7 @@ const InvolvementStep = withForm({
           </form.AppField>
         </form.Question>
 
-        <form.Question
-          question="Skills & Activities Interest"
-          density="compact"
-        >
+        <form.Question question="Skills & Activities Interest">
           <form.AppField name="involvement.skills">
             {(field) => (
               <field.MultiSelect className="w-full" options={SKILL_OPTIONS} />
@@ -72,7 +69,7 @@ const InvolvementStep = withForm({
         </form.Question>
 
         <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
-          <form.Question question="Gender Identity" density="compact">
+          <form.Question question="Gender Identity">
             <form.AppField name="involvement.gender">
               {(field) => (
                 <field.RadioGroup className="w-full" options={GENDER_OPTIONS} />
@@ -96,7 +93,7 @@ const InvolvementStep = withForm({
             </form.Subscribe>
           </form.Question>
 
-          <form.Question question="Preferred Time Commitment" density="compact">
+          <form.Question question="Preferred Time Commitment">
             <form.AppField name="involvement.timeCommitment">
               {(field) => (
                 <field.RadioGroup
