68b34f3f7f5c70cdf8bd7b58749323240f6a63cd
Justin <justinschwerdtfeger245@gmail.com>
2026-08-25 11:55:29 -0500

    Make FormQuestion able to take ReactNode

--- src/lib/components/form/FormQuestion.tsx
+++ src/lib/components/form/FormQuestion.tsx
@@ -7,7 +7,7 @@ type FormQuestionProps = {
   /**
    * Text for the question
    */
-  question?: string;
+  question?: string | ReactNode;
   /**
    * Amount of space between the question text and contents
    * @default "full"
@@ -26,7 +26,11 @@ export default function FormQuestion({
       className={`flex flex-col ${density === 'full' ? 'gap-6' : 'gap-2'} ${className}`}
     >
       <div className="flex flex-col gap-2">
-        <Typography variant="body1">{question}</Typography>
+        {typeof question === 'string' ? (
+          <Typography variant="body1">{question}</Typography>
+        ) : (
+          question
+        )}
       </div>
       <div className="flex w-full flex-wrap gap-6">{children}</div>
     </div>
