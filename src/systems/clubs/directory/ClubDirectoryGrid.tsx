42ce1c68c06aa94756c76ba978894d1d5dc71476
Isosc <171582725+Isoscelestial@users.noreply.github.com>
2026-08-24 19:35:14 -0500

    feat: club match ad on landing page

--- src/systems/clubs/directory/ClubDirectoryGrid.tsx
+++ src/systems/clubs/directory/ClubDirectoryGrid.tsx
@@ -2,11 +2,28 @@
 
 import { keepPreviousData, useQuery } from '@tanstack/react-query';
 import { AnimatePresence, motion } from 'framer-motion';
-import { useEffect } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 import { useSearchStore } from '@/systems/dashboard/SearchStoreProvider';
 import { useTRPC } from '@/trpc/react';
 import ClubCard, { ClubCardSkeleton } from '../ClubCard';
 import InfiniteScrollGrid from './InfiniteScrollGrid';
+import ClubMatchAdCard, {
+  ClubMatchAdDismissedOnStorageKey,
+} from './ads/ClubMatchAdCard';
+import { RouterOutputs } from '@/trpc/shared';
+import { subDays } from 'date-fns';
+
+type Cards = (
+  | {
+      type: 'club';
+      club: RouterOutputs['club']['search']['clubs'][number];
+    }
+  | {
+      type: 'ad';
+      key: string;
+      render: ReactNode;
+    }
+)[];
 
 const ClubDirectoryGrid = () => {
   const {
@@ -43,6 +60,37 @@ const ClubDirectoryGrid = () => {
     setSearchBarLoading,
   ]);
 
+  // ClubMatchAdCard stays dismissed for up to 7 days
+  const [dismissedClubMatchAd, setDismissedClubMatchAd] = useState<boolean>(
+    () => {
+      let storageValue: string | null = null;
+      if (typeof window !== 'undefined') {
+        storageValue = window.localStorage.getItem(
+          ClubMatchAdDismissedOnStorageKey,
+        );
+      }
+
+      if (!storageValue) return false;
+      const storageDate = new Date(storageValue);
+      const oneWeekAgo = subDays(new Date(), 7);
+      return storageDate.getTime() > oneWeekAgo.getTime();
+    },
+  );
+
+  const cards: Cards | undefined = data?.clubs.map((club) => ({
+    type: 'club',
+    club: club,
+  }));
+
+  if (cards && !dismissedClubMatchAd) {
+    const placeAtEnd = search !== '' || tags.length !== 0;
+    cards.splice(placeAtEnd ? cards.length : 1, 0, {
+      type: 'ad',
+      key: 'club-match-ad',
+      render: <ClubMatchAdCard setDismissed={setDismissedClubMatchAd} />,
+    });
+  }
+
   return (
     <div
       className={`grid w-full auto-rows-fr grid-cols-[repeat(auto-fill,320px)] justify-center gap-16 pb-4 transition-opacity duration-300 ${!showNoResults && 'pb-40'}`}
@@ -59,9 +107,9 @@ const ClubDirectoryGrid = () => {
         </div>
       ) : (
         <AnimatePresence mode="popLayout">
-          {data?.clubs.map((club) => (
+          {cards?.map((card) => (
             <motion.div
-              key={club.id}
+              key={card.type === 'club' ? card.club.id : card.key}
               layout="position"
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
@@ -74,7 +122,11 @@ const ClubDirectoryGrid = () => {
               }}
               className="h-full w-full"
             >
-              <ClubCard club={club} priority />
+              {card.type === 'club' ? (
+                <ClubCard club={card.club} priority />
+              ) : (
+                card.render
+              )}
             </motion.div>
           ))}
           {/* Only show infinite scroll if not fetching */}
---
+++ src/systems/clubs/directory/ads/ClubMatchAdCard.tsx
@@ -0,0 +1,67 @@
+'use client';
+
+import { LinkButton } from '@/lib/components/LinkButton';
+import { Binoculars } from '@/lib/icons/OtherIcons';
+import Button from '@mui/material/Button';
+import { BaseCard } from '@nebula-library/components/BaseCard';
+import Link from 'next/link';
+
+export const ClubMatchAdDismissedOnStorageKey = 'ClubMatchAdDismissedOn';
+
+export default function ClubMatchAdCard({
+  setDismissed,
+}: {
+  setDismissed?: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
+  return (
+    <BaseCard
+      variant="interactive"
+      className="bg-cornflower-100 dark:bg-cornflower-900 relative flex h-full min-h-[400px] max-w-xs min-w-[300px] flex-col justify-between p-6 md:min-h-[600px]"
+    >
+      <Link
+        href={`/club-match/form`}
+        className="ClubMatchAdCardLink absolute inset-0 z-10"
+      />
+
+      <div className="flex aspect-square w-full items-center justify-center">
+        <Binoculars className="text-8xl opacity-50" />
+      </div>
+      <div className="flex flex-col gap-6">
+        <p className="line-clamp-2 text-xl font-medium text-slate-800 dark:text-slate-200">
+          Club Match
+        </p>
+        <p>
+          Want to quickly find clubs that match your hobbies and interests? Take
+          this quiz and get intelligently matched with student organizations at
+          UTD that you may find interesting!
+        </p>
+        <LinkButton
+          href="/club-match/form"
+          size="large"
+          color="primary"
+          variant="contained"
+          startIcon={<Binoculars />}
+          className="z-20"
+        >
+          Start now!
+        </LinkButton>
+        {setDismissed && (
+          <Button
+            size="large"
+            color="inherit"
+            className="z-20"
+            onClick={() => {
+              window.localStorage.setItem(
+                ClubMatchAdDismissedOnStorageKey,
+                new Date().toISOString(),
+              );
+              setDismissed(true);
+            }}
+          >
+            Dismiss
+          </Button>
+        )}
+      </div>
+    </BaseCard>
+  );
+}
