356d1b6d0755d7bc5799306de10f503862e9eeb7
Isosc <171582725+Isoscelestial@users.noreply.github.com>
2026-09-19 06:59:11 -0500

    feat: update BetterAuth to 1.7 for hydrateSession, improve design and loading for ProfileDropDown

--- src/lib/modules/navigation/header/ProfileDropDown.tsx
+++ src/lib/modules/navigation/header/ProfileDropDown.tsx
@@ -1,9 +1,11 @@
 'use client';
 
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import LogoutIcon from '@mui/icons-material/Logout';
 import SettingsIcon from '@mui/icons-material/Settings';
 import {
   Avatar,
+  Button,
   Card,
   ListItemIcon,
   ListItemText,
@@ -20,13 +22,42 @@ import { Binoculars } from '@/lib/icons/OtherIcons';
 import { openLoginModal } from '@/lib/modules/loginModal';
 import { authClient } from '@/lib/utils/auth-client';
 import { getAvailableSocialProviders } from '@/lib/utils/socialProviders';
+import { auth } from '@/server/auth';
 
-type Props = {
+type ProfileDropDownProps = {
   shadow?: boolean;
+  initialSession?: typeof auth.$Infer.Session | null;
 };
 
-export const ProfileDropDown = ({ shadow = false }: Props) => {
-  const { data: session, isPending } = authClient.useSession();
+export function ProfileDropDownFallback({
+  shadow = false,
+}: ProfileDropDownProps) {
+  return (
+    <Button
+      variant="contained"
+      color="inherit"
+      disableElevation
+      startIcon={
+        <AccountCircleOutlinedIcon fontSize="large" className="text-2xl" />
+      }
+      className={`h-10 bg-slate-100 px-5 py-2 whitespace-nowrap text-slate-950 normal-case hover:bg-slate-200 dark:bg-slate-300 dark:hover:bg-slate-400 ${shadow ? 'drop-shadow-[0_0_4px_rgb(0_0_0_/_0.4)]' : ''}`}
+    >
+      Sign in
+    </Button>
+  );
+}
+
+export default function ProfileDropDown({
+  shadow = false,
+  initialSession,
+}: ProfileDropDownProps) {
+  useEffect(() => {
+    if (initialSession) authClient.hydrateSession(initialSession);
+  }, [initialSession]);
+
+  const { data, isPending, isRefetching } = authClient.useSession();
+  const session = isPending || !isRefetching ? initialSession : data;
+
   const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
   const open = Boolean(anchorEl);
 
@@ -54,23 +85,42 @@ export const ProfileDropDown = ({ shadow = false }: Props) => {
     }
   }, [open]);
 
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    if (session !== null) {
+      setAnchorEl(open ? null : e.currentTarget);
+    } else {
+      openLoginModal();
+    }
+  };
+
   return (
     <>
-      <Avatar
-        alt={isPending ? undefined : session?.user.name}
-        src={isPending ? undefined : (session?.user.image ?? undefined)}
-        onClick={(e) => {
-          if (session !== null) {
-            setAnchorEl(open ? null : e.currentTarget);
-          } else {
-            openLoginModal();
+      {session ? (
+        <Avatar
+          alt={session.user.name}
+          src={session.user.image ?? undefined}
+          onClick={handleClick}
+          onMouseEnter={handlePrefetchAvailableSocialProviders}
+          onFocus={handlePrefetchAvailableSocialProviders}
+          component="button"
+          className={`cursor-pointer ${shadow ? 'drop-shadow-[0_0_4px_rgb(0_0_0_/_0.4)]' : ''}`}
+        >
+          {session.user.name.charAt(0)}
+        </Avatar>
+      ) : (
+        <Button
+          variant="contained"
+          color="inherit"
+          disableElevation
+          onClick={handleClick}
+          startIcon={
+            <AccountCircleOutlinedIcon fontSize="large" className="text-2xl" />
           }
-        }}
-        onMouseEnter={handlePrefetchAvailableSocialProviders}
-        onFocus={handlePrefetchAvailableSocialProviders}
-        component="button"
-        className={`cursor-pointer ${shadow ? 'drop-shadow-[0_0_4px_rgb(0_0_0_/_0.4)]' : ''}`}
-      />
+          className={`h-10 bg-slate-100 px-5 py-2 whitespace-nowrap text-slate-950 normal-case hover:bg-slate-200 dark:bg-slate-300 dark:hover:bg-slate-400 ${shadow ? 'drop-shadow-[0_0_4px_rgb(0_0_0_/_0.4)]' : ''}`}
+        >
+          Sign in
+        </Button>
+      )}
       {!isPending && session && (
         <Popover
           open={open}
@@ -137,4 +187,4 @@ export const ProfileDropDown = ({ shadow = false }: Props) => {
       )}
     </>
   );
-};
+}
