059388134136ce4827d3de6a61b037daa5922e88
Ahmed Hisham <hishamahmed312@gmail.com>
2026-09-24 19:33:26 -0500

    clubs\JoinButton.tsx: removed unneeded comments I added for myself

--- src/systems/clubs/JoinButton.tsx
+++ src/systems/clubs/JoinButton.tsx
@@ -103,8 +103,7 @@ const JoinButton = ({ isHeader, clubId, clubSlug }: JoinButtonProps) => {
     },
   });
 
-  const memberType = memberState?.memberType ?? null; // test
-  //const memberType = 'President';
+  const memberType = memberState?.memberType ?? null; 
 
   if (memberType === 'Officer' || memberType === 'President') {
     return (
